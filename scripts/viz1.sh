aa85d18049918fb2927726e9ade69b51985a8374
Alejandro Sherman <alejandro.sherman100@gmail.com>
2021-11-17 10:04:00 -0800

    Main Script now uses the paste command

--- scripts/viz1.sh
+++ scripts/viz1.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 #sizeinfo=$( cat find.txt | awk '{print $5}' )
 #locationinfo=$( cat find.txt | awk -F/ 'OFS="/" {print $3, $4}' )
-allinfo=$( cat find.txt | awk -F'[ /]' 'OFS="/" {print $5, $(NF-1), $(NF)}' )
+#allinfo=$( cat find.txt | awk -F'[ /]' 'OFS="/" {print $5, $(NF-1), $(NF)}' )
+allinfo=$( cat find.txt | awk -F'[ /]' 'OFS="/" {print $5, $(NF-1), $(NF)}' | sort -nrk1 )
 parentinfo=$( cat find.txt | awk -F'[ /]' '{print $(NF-1)}' )
 pinfo=$( awk 'BEGIN{ORS=" "}{for (i=1;i<=NF; i++)a[$i]++} END {for (i in a) print i}' <<< "$parentinfo " );
 #pinfo=$( awk 'BEGIN{RS=ORS=" "}{if (a[$0] == 0){ a[$0] += 1; print $0}}' <<< $parentinfo );
+folder=$"temp_files/";
+folde=$"./temp_files/";
 
 make_rectangle () {
    #echo "Current Parent folder is: $1"
@@ -70,19 +73,37 @@ for PARENT in $pinfo
 do 
    #echo "Current Parent folder is: $PARENT"
    make_rectangle "$PARENT"
-   for INFO in $allinfo
+   for INFO in $allinfo #this loop creates the circle files paste will use
    do 
+      #echo "$INFO"
       size=$( echo "$INFO" | awk -F/ '{print $1}' )
       #parent=$( echo "$INFO" | awk -F/ '{print $2}' )
       file=$( echo "$INFO" | awk -F/ '{print $3}' )
+      createfile="$folder$file"
       #prefile=${file%[0-9]*.*}
       #if [[ $PARENT == *"$prefile"* ]];
-      if [[ $INFO == *"$PARENT"* ]];
+      if [[ $INFO == *"$PARENT"* ]]; #we found a file inside of this folder
       then
          #echo "$file here of size $size."
-         draw_circle "$size" "$file"
+         draw_circle "$size" "$file" > "$createfile"
+         #echo "file to create is : $createfile"
       fi
    done
+   #read -p "Hit any key to continue."
+   string=$""
+   for INF in $allinfo #this loop performs the paste commands
+   do 
+      siz=$( echo "$INF" | awk -F/ '{print $1}' )
+      fil=$( echo "$INF" | awk -F/ '{print $3}' )
+      createfil="$folder$fil"
+      if [[ $INF == *"$PARENT"* ]]; #we found the file again
+      then
+          string+=$( echo "$createfil "  )
+      fi
+   done
+   echo "Now we pass this to paste: $string"
+   #cat ${string::-1}
+   paste ${string::-1}
    read -p "Hit any key to continue."
 done
 echo "Done" 
